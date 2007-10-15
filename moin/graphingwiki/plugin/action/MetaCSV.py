4410eed85bb4bbbff0e97348dfec83a3e7112b50
Erno Kuusela <erno@iki.fi>
2007-10-15 19:13:09 +0000

    MetaCSV fixing

--- moin/graphingwiki/plugin/action/MetaCSV.py
+++ moin/graphingwiki/plugin/action/MetaCSV.py
@@ -11,24 +11,31 @@ from graphingwiki.editing import process_edit, getvalues
 from graphingwiki.editing import metatable_parseargs
 
 def execute(pagename, request):
-    request.http_headers(['Content-Type: text/plain',
-                          'Not-Content-Disposition: attachment; filename="%s.csv"' % pagename])
+    request.http_headers(['Content-Type: text/csv',
+                          'Content-Disposition: attachment; filename="%s.csv"' % pagename])
     GetMeta = wikiutil.importPlugin(request.cfg, 'xmlrpc', 'GetMeta')
     class x: pass
     x.request = request
-
-    z = GetMeta(x, pagename, keysonly=False)
-    if 0: print '--', z, '--'
+    try:
+        args = request.args['args'][0]
+    except (KeyError, IndexError):
+        args = u''
+    z = GetMeta(x, args, keysonly=False)
+    if 0:
+        print '--', z, '--'
+        print 'args', request.args
+        print 'pagename', pagename
     keys = ['page name'] + z.pop(0)
     writer = csv.writer(request)
     writer.writerow(keys)
+
     for row in z:
-        # why are the values lists?
+        # multiple values will get concatenated on one row, joined with ','
         def val2text(val):
             if not val:
                 return ''
             else:
-                out = u' '.join(val)
+                out = u', '.join(val)
                 return out.encode('utf-8')
         row[1:] = map(val2text, row[1:])
         writer.writerow(row)
