bfffb57ec403b38d6c6149d6dd79b6a504b39f54
Joachim Viide <jviide@iki.fi>
2008-02-28 17:04:14 +0000

    Added "reassembly" mode for chunked uploading.

--- moin/graphingwiki/plugin/xmlrpc/ChunkedAttachFile.py
+++ moin/graphingwiki/plugin/xmlrpc/ChunkedAttachFile.py
@@ -1,7 +1,16 @@
+import os
 import xmlrpclib
 import md5
 
+from tempfile import mkdtemp
+from shutil import rmtree
+
+from graphingwiki.editing import save_attachfile
+from graphingwiki.editing import load_attachfile
 from graphingwiki.editing import check_attachfile
+from graphingwiki.editing import delete_attachfile
+
+from graphingwiki.editing import list_attachments
 
 CHUNK_SIZE = 1024 * 1024
 
@@ -57,18 +66,104 @@ def load(request, pagename, filename, start, end):
 
     return xmlrpclib.Binary(data)
 
-def execute(xmlrpcobj, pagename, filename, action='info', 
-            start=None, end=None):
+def reassembly(request, pagename, filename, chunkSize, digests, overwrite=True):
+    _ = request.getText
+
+    # Using the same access controls as in MoinMoin's xmlrpc_putPage
+    # as defined in MoinMoin/wikirpc.py
+    if (request.cfg.xmlrpc_putpage_trusted_only and
+        not request.user.trusted):
+        return xmlrpclib.Fault(1, _("You are not allowed to attach a "+
+                                    "file to this page"))
+
+    # Also check ACLs
+    if not request.user.may.write(pagename):
+        return xmlrpclib.Fault(1, _("You are not allowed to attach a "+
+                                    "file to this page"))
+
+    # Check whether the file already exists. If it does, check the hashes.
+    fpath, exists = check_attachfile(request, pagename, filename)
+    if chunkSize is not None and exists:
+        try:
+            stream = open(fpath, "rb")
+        except:
+            pass
+        else:
+            for digest in digests:
+                data = stream.read(chunkSize)
+                other = md5.new(data).hexdigest()
+                if other != digest:
+                    break
+            else:
+                data = stream.read(chunkSize)
+                if not data:
+                    stream.close()
+                    return list()
+            
+            stream.close()
+
+        # Fail if the file doesn't match and we don't want to overwrite.
+        if overwrite:
+            return xmlrpclib.Fault(2, _("Attachment not saved, file exists"))
+
+    # If there are missing chunks, just return them.
+    result = list_attachments(request, pagename)
+    missing = [digest for digest in digests if digest not in result]
+    if missing:
+        return missing
+
+    # Reassembly the file from the chunks into a temp file.
+    tmpDir = mkdtemp()
+    tmpPath = os.path.join(tmpDir, filename)
+    try:
+        tmp = file(tmpPath, 'wb')
+    except:
+        return xmlrpclib.Fault(3, _("Could not create temp file"))
+    
+    try:
+        for bite in digests:
+            data = load_attachfile(request, pagename, bite)
+            if not data:
+                return xmlrpclib.Fault(2, "%s: %s" % (_("Nonexisting "+
+                                                        "attachment"),
+                                                      filename))
+            tmp.write(data)
+    except:
+        return xmlrpclib.Fault(3, _("Unknown error"))
+    finally:
+        tmp.close()
+
+    # Attach the decoded file.
+    success = save_attachfile(request, pagename, tmpPath, filename, overwrite)
+    rmtree(tmpDir)
+
+    # FIXME: What should we do when the cleanup fails?
+    for bite in digests:
+        delete_attachfile(request, pagename, bite)
+
+    # On success signal that there were no missing chunks.
+    if success is True:
+        return list()
+    
+    if overwrite == False:
+        return xmlrpclib.Fault(2, _("Attachment not saved, file exists"))
+
+    return xmlrpclib.Fault(3, _("Unknown error while attaching file"))
+
+def execute(xmlrpcobj, page, file, action='info', start=None, end=None):
     request = xmlrpcobj.request
     _ = request.getText
 
-    pagename = xmlrpcobj._instr(pagename)
+    page = xmlrpcobj._instr(page)
 
     if action == 'info':
-        success = runChecked(info, request, pagename, filename)
+        success = runChecked(info, request, page, file)
     elif action == 'load' and None not in (start, end):
-        success = runChecked(load, request, pagename, filename, start, end)
+        success = runChecked(load, request, page, file, start, end)
+    elif action == 'reassembly' and start is not None:
+        chunk, digests = start, end
+        success = runChecked(reassembly, request, page, file, chunk, digests)
     else:
         success = xmlrpclib.Fault(3, _("No method specified or invalid span"))
 
-    return success
+    return map(unicode, success)
