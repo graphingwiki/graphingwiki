3a185defd64f44bb7c3ccfad45acbd78b93fa614
Lari Huttunen <github@huttu.net>
2008-03-19 21:19:15 +0000

    Changed the package_dir back to the original due to public demand. ;)

--- opencollab/setup.py
+++ opencollab/setup.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 
-setup(name='opencollab', version='355',
+setup(name='opencollab', version='360',
       author='Joachim Viide, Pekka Pietikäinen, Mika Seppänen',
       author_email='contact@clarifiednetworks.com',
       description='OpenCollab XML-RPC SDK',
       packages=['opencollab'],
-      package_dir={'opencollab': 'lib'},
-      package_data={'': ['lib/*.py']},
+      package_dir={'': 'opencollab'},
+      package_data={'opencollab': ['opencollab/*.py']},
       scripts=['scripts/opencollab-uploader', 
         'scripts/opencollab-downloader',
         'scripts/opencollab-push-tickets'])
