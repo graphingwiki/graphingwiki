e116616447620f3a6b2045f3b8bcd1040d2612c6
Lari Huttunen <github@huttu.net>
2008-03-24 18:59:58 +0000

    Removed package_dir, since it is not needed.

--- opencollab/setup.py
+++ opencollab/setup.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 
-setup(name='opencollab', version='360',
+setup(name='opencollab', version='382',
       author='Joachim Viide, Pekka Pietikäinen, Mika Seppänen',
       author_email='contact@clarifiednetworks.com',
       description='OpenCollab XML-RPC SDK',
       packages=['opencollab'],
-      package_dir={'opencollab': 'opencollab'},
-      package_data={'opencollab': ['opencollab/*.py']},
+      package_data={'opencollab': ['*.py']},
       scripts=['scripts/opencollab-uploader', 
         'scripts/opencollab-downloader',
         'scripts/opencollab-delete-pages',
