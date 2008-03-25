2e5ffcbeefd01ae2a46bb174455a020425aa1624
Lari Huttunen <github@huttu.net>
2008-03-25 17:10:53 +0000

    Added a new script, opencollab-create-gallery, which will be used as a prototype for image gallery wautomation. This script is still mostly vaporware. ;)

--- opencollab/setup.py
+++ opencollab/setup.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 
-setup(name='opencollab', version='382',
+setup(name='opencollab', version='386',
       author='Joachim Viide, Pekka Pietikäinen, Mika Seppänen',
       author_email='contact@clarifiednetworks.com',
       description='OpenCollab XML-RPC SDK',
       packages=['opencollab'],
       package_data={'opencollab': ['*.py']},
-      scripts=['scripts/opencollab-uploader', 
-        'scripts/opencollab-downloader',
+      scripts=['scripts/opencollab-clone-pages',
+        'scripts/opencollab-create-gallery',
         'scripts/opencollab-delete-pages',
-        'scripts/opencollab-push-tickets'])
+        'scripts/opencollab-downloader',
+        'scripts/opencollab-push-tickets',
+        'scripts/opencollab-uploader'])
 
