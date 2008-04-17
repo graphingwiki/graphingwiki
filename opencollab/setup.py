1e6eca222d90c303bd83c178b8f55b97493ea45d
Lari Huttunen <github@huttu.net>
2008-04-17 09:33:19 +0000

    Added opencollab-agent to setup.py and license to agent.

--- opencollab/setup.py
+++ opencollab/setup.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 
-setup(name='opencollab', version='386',
+setup(name='opencollab', version='397',
       author='Joachim Viide, Pekka Pietikäinen, Mika Seppänen',
       author_email='contact@clarifiednetworks.com',
       description='OpenCollab XML-RPC SDK',
       packages=['opencollab'],
       package_data={'opencollab': ['*.py']},
-      scripts=['scripts/opencollab-clone-pages',
+      scripts=[ 'scripts/opencollab-agent',
+        'scripts/opencollab-clone-pages',
         'scripts/opencollab-create-gallery',
         'scripts/opencollab-delete-pages',
         'scripts/opencollab-downloader',
