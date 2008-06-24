ada0f3a4843017c61266efd8370f961b6491c028
Lauri Pokka <larpo@iki.fi>
2008-06-24 07:55:15 +0000

    Dynamic UI
    --HG--
    branch : raippa

--- action/editQuestion.py
+++ action/editQuestion.py
@@ -59,38 +59,253 @@ def do_upload(request, pagename):
         stream.close()
 
 def show_basicform(request):
-#TODO: javasciprt: add more answers dynamically
-#TODO: javascript: create answer UI dynamically depending on aswer type
- #TODO: javascript: if answer type is file, show select for manual or automatic checking
- #TODO: javascript: hide tip and show it only if answer value is False
     html = str()
     html += u'''
-<form method="POST" enctype="multipart/form-data">
-    <input type="hidden" name="action" value="%s">
-    question: <input type="text" name="question"><br>
-    note: <input type="text" name="note"><br>
-    image: <input type="file" name="file"/><br>
-    <hr>
-    answer type: <select name="answertype">
-        <option value="radio">radio
-        <option value="checkbox">checkbox
-        <option value="text">text
-        <option value="regexp">regexp
-        <option value="file">file
-    </select>''' % action_name
+ <script type="text/javascript">
+/*	Using mootools javascript framework */
 
-    for answernumber in range(1,6):
+/* Set object given in parameters visible */
+function show(obj){
+    var tr = $(obj).getParent('tr');
+	tr.getElement('span').style.visibility = "";
+ }
+ /* Hide given object */
+function hide(obj){
+    var tr = $(obj).getParent('tr');
+	tr.getElement('span').style.visibility = "hidden";
+}
+var deleted = 0;
+
+/* Generates answerfield dynamically and sets right visibility values  */
+function addField(){
+
+var sel = document.getElementById('typeSelect').value;
+var lkm = deleted;
+var to = document.getElementById('ansRow').tBodies[0];
+    for(var i = 0; i< to.childNodes.length; i++){
+       if(to.childNodes[i].tagName){
+	      if(to.childNodes[i].tagName == "TR"){
+		     lkm++;
+		  }
+		}
+	}
+
+var newRow = new Element('tr');
+var td1 = new Element('td');
+var td2 = new Element('td');
+var td3 = new Element('td', { 'class' : 'rexp' });
+var td4 = new Element('td', { 'class' : 'rexp' });
+var td5 = new Element('td');
+var td6 = new Element('td');
+var td7 = new Element('td');
+
+td1.grab(new Element('input', {
+	'type' : 'checkbox',
+	'name' : 'rmcheck'+lkm,
+	'value' : 'rm',
+	'title' : 'Select this answer to be deleted'
+  })
+);
+
+td2.grab(new Element('input', {
+	'type' : 'text',
+	'name' : 'answer'+lkm
+  })
+);
+
+td3.grab(new Element('input' , {
+	'type' : 'checkbox',
+	'name' : 'cSens'+lkm,
+	'value' : 'true',
+	'title' : 'Answer is case sensitive'
+  })
+);
+
+
+td4.grab(new Element('input' , {
+	'type' : 'checkbox',
+	'name' : 'rexp'+lkm,
+	'value' : 'true',
+	'title' : 'Answer is a regular expression'
+  })
+);
+var radioTrue = new Element('input', {
+	'type' : 'radio',
+	'name' : 'value'+lkm,
+	'value' : 'true',
+	'checked': 'true'
+});
+td5.grab(radioTrue);
+var radioFalse = new Element('input', {
+	'type' : 'radio',
+	'name' : 'value'+lkm,
+	'value' : 'false'
+});
+
+td6.grab(radioFalse);
+var span = new Element('span', {
+	'class' : 'tip',
+	'style' : 'visibility: hidden;'
+  });
+
+span.set('text', 'Tip: ');
+
+span.grab(new Element('input', {
+	'type' : 'text',
+	'name' : 'tip' + lkm
+  })
+);
+
+td7.grab(span);
+
+newRow.adopt(td1, td2, td3, td4, td5, td6, td7);
+
+/* Add tip-field visibility controlling */
+td5.getFirst('input').addEvent('click', function(){
+	var tr = this.getParent('tr');
+	tr.getElement('span').style.visibility = "hidden";
+  });
+
+td6.getFirst('input').addEvent('click', function(){
+	var tr = this.getParent('tr');
+	tr.getElement('span').style.visibility = "";
+  });
+
+$('ansRow').getFirst('tbody').grab(newRow);
+
+//make rexp classes visible if needed
+typeCheck();
+}
+
+
+/* Removes selected answer fields */
+function rmField(){
+var input, name, value, type, checked;
+var tab = document.getElementById('ansRow').tBodies[0];
+var regExp = /rmcheck\d+/;
+var done = 0;
+
+while(done != 1){
+done = 1;
+var checks = document.getElementsByTagName('INPUT');
+for(var i in checks){
+    try{
+		input = checks[i];
+        name = input.getAttribute('name');
+        value = input.getAttribute('value');
+        type = input.getAttribute('type');
+        if(type == "checkbox"){
+		  checked = input.checked;
+		  }
+
+	  if(regExp.test(name) == true && checked == true){
+	  /* Create tr to keep field adder in track */
+	  tab.removeChild(input.parentNode.parentNode);
+      done = 0;
+      deleted++;
+	  break;
+        }
+    }catch(err){
+	  }
+  }
+}
+}
+
+/* Change visibility of answer data fields depending on which answertype has
+been selected */
+function typeCheck(){
+var sel = document.getElementById('typeSelect').value;
+var tds = document.getElementsByTagName('TD');
+var td = null;
+  if(sel === "file"){
+	document.getElementById('ansRow').style.display = "none";
+	document.getElementById('fieldCreator').style.display = "none";
+
+ }else{
+	document.getElementById('ansRow').style.display = "";
+  	document.getElementById('fieldCreator').style.display = "";
+  
+
+  }
+
+for(var i in tds){
+  if(tds[i]){
+	td = tds[i];
+    if(td.className === "rexp" && sel !== "text"){
+	  td.style.display = "none";
+    }else if(td.className === "rexp" && sel === "text"){
+	  td.style.display = "";
+    }
+  }
+}
+}
+</script>
+
+<form method="POST" enctype="multipart/form-data">
+<input type="hidden" name="action" value="%s">
+<table style="border-style:hidden">
+<tr style="border-style:hidden"><td  style="border-style:hidden">question:</td><td style="border-style:hidden"colspan="2"> <input type="text" name="question"></td><tr>
+<tr style="border-style:hidden"><td  style="border-style:hidden">note:</td><td colspan="2"style="border-style:hidden"> <input type="text" name="note"></td><tr>
+<tr style="border-style:hidden"><td  style="border-style:hidden">image:</td><td colspan="2"style="border-style:hidden">
+<input type="file" name="file" value="Select..."></td><tr>
+</table>
+    <hr>
+    answer type: &nbsp; <select id="typeSelect" name="answertype"
+    onChange="typeCheck();">
+        <option value="radio">radio</option>
+        <option value="checkbox">checkbox</option>
+        <option value="text">text</option>
+        <option value="file">file</option>
+    </select> &nbsp; 
+    <input id="fieldCreator" type="button" name="addButton" title="Add more answer fields"
+    value="Add new field" onClick="addField();">
+<br>
+ <table id="ansRow">
+<tr>
+    <td style="text-align:center"><a title="Remove selected answers"
+	style="color:red"  href="javascript: rmField();">X</a></td>
+    <td style="width:150px">Answer:</td>
+    <td title="Case sensitive" class="rexp" style="width:110px; text-align:
+    center">Case sensitive</td>
+<td title="Regular Expression" class="rexp" style="width:75px; text-align:
+    center">Regexp</td>
+    <td style="width:50px">Right</td>
+    <td style="width:50px">Wrong</td>
+    <td></td>
+</tr>
+  ''' % action_name
+
+    for answernumber in range(1,5):
         html += '''
-    <br>answer: <input type="text" name="answer%s">
-    <input type="radio" name="value%s" value="true" checked>True
-    <input type="radio" name="value%s" value="false">False
-    tip: <input type="text" name="tip%s">
-''' % (answernumber, answernumber, answernumber, answernumber)
+<tr id="ansTr%s"><td><input type="checkbox" name="rmcheck%s" value="rm" title="Select this
+answer to be deleted"></td>
+    <td> <input type="text" name="answer%s"></td>
+    <td class="rexp"><input type="checkbox" name="cSens%s"  value="true" 
+    	title="Answer is case sensitive"></td>
+    <td class="rexp"><input type="checkbox" name="rexp%s"value="true"
+    	title="Answer is regular expression" ></td>
+    <td><input type="radio" name="value%s" value="true"
+    	onClick="hide(this);" checked ></td>
+    <td><input type="radio" name="value%s" value="false" 
+    	onClick="show(this);"></td>
+    <td><span class="tip">Tip: <input type="text" name="tip%s"></span></td>
+</tr>
+
+
+''' % (answernumber, answernumber, answernumber, answernumber, answernumber, answernumber, answernumber, answernumber)
 
     html += u'''
+  </table>
     <hr>
-    <input type="submit" name="save" value="Save">
-</form>'''
+     <input type="submit" name="save" value="Save">
+    </form>
+<script type="text/javascript">
+/* Hide tip-fields, regexp and casesensitive checkboxes on pageload */
+
+typeCheck();
+$('ansRow').getElements('span').set('style', 'visibility:hidden');
+</script>
+'''
 
     request.write(html)
 
@@ -204,7 +419,8 @@ def _enter_page(request, pagename):
     request.http_headers()
     _ = request.getText
     
-    request.theme.send_title(_('Teacher Tools'), formatted=False)
+    request.theme.send_title(_('Teacher Tools'), formatted=False,
+	html_head='<script type="text/javascript" src="/moin_static163/common/js/mootools-1.2-core.js"></script>')
     if not hasattr(request, 'formatter'):
         formatter = HtmlFormatter(request)
     else:
