945a7516d5e1207c0cc2ceecf69c2388befec8f6
Rauli Leevi Sakari Puuperä <therauli@therau.li>
2008-08-18 08:44:42 +0000

    updatet showCalendarDate action. Nearly ready
    --HG--
    branch : moin-1.6-branch

--- graphingwiki/graphingwiki/plugin/action/showCalendarDate.py
+++ graphingwiki/graphingwiki/plugin/action/showCalendarDate.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import datetime
 
 from MoinMoin.Page import Page
@@ -8,7 +10,7 @@ from graphingwiki.editing import metatable_parseargs, getmetas
 
 def _enter_page(request, pagename):
     _ = request.getText
-
+	   
     title = _('Calendar')
 
     request.theme.send_title(title,
@@ -21,11 +23,11 @@ def _enter_page(request, pagename):
         formatter = HtmlFormatter(request)
     else:
         formatter = request.formatter
-
+    
     request.page.formatter = formatter
 
     request.write(request.page.formatter.startContent("content"))
-
+    
 def _exit_page(request, pagename):
     # End content
     request.write(request.page.formatter.endContent()) # end content div
@@ -33,10 +35,10 @@ def _exit_page(request, pagename):
     request.theme.send_footer(pagename)
 
 def addEntry(pagename, date, request):
-    request.write('<a href="%s?action=edit&backto=%s">Add entry</a>' % (date, pagename))
+    request.write('<br><a href="%s?action=edit&backto=%s">Add entry</a>' % (date, pagename))
 
-
-def printEvents(events, date, pagename, request):
+      
+def printEntries(entries, date, pagename, request):
 
     def writeCell(stuff, width=''):
         if width:
@@ -48,68 +50,149 @@ def printEvents(events, date, pagename, request):
 
     backto = request.form.get('backto', [None])[0].encode()
 
+
     request.write(u'<table>')
     request.write(u'<th colspan=4>%s</th>' % date)
 
-    for event in events:
-        request.write('<tr>')
-        startTime = datetime.datetime.strptime(event['Date'][0][0] + ' ' + event['Time'][0][0], '%Y-%m-%d %H:%M')
-        hours = int(event['Duration'][0][0].split(':')[0])
-        minutes = int(event['Duration'][0][0].split(':')[1])
-        endTime = startTime + datetime.timedelta(hours = hours, minutes = minutes)
-        writeCell(startTime.strftime('%H:%M - ') + endTime.strftime('%H:%M'))
+    for day in entries:
+        if day != date:
+            continue
 
-        #Parser('== foo ==', request).format(request.formatter)
-        writeCell(request.formatter.text(event['Content']), width="50%")
+        print entries[day]
+        
+        
+        for entry in entries[day]:
 
-        writeCell('<a href="%s?action=edit&backto=%s">edit</a>' % (event['Page'], backto)) #request.request_uri[1:]))
-        writeCell('<a href="%s?action=DeletePage">remove</a>' % event['Page'])
+            request.write('<tr>')
+            startTime = None
 
-        request.write('</tr>')
+            if 'Time' in entry:
+                startTime = datetime.datetime.strptime(entry['Date'] + ' ' + entry['Time'], '%Y-%m-%d %H:%M')
+            endTime = None
+            if 'Duration' in entry:
+                hours = int(entry['Duration'].split(':')[0])
+                minutes = int(entry['Duration'].split(':')[1])
+                endTime = startTime + datetime.timedelta(hours = hours, minutes = minutes)
+            if startTime and endTime:
+                writeCell(startTime.strftime('%H:%M') + endTime.strftime(' - %H:%M'))
+            elif startTime:
+                writeCell(startTime.strftime('%H:%M'))
+            else:
+                writeCell('?')
 
+            writeCell(request.formatter.text(entry['Content']+ ' ' + entry['Date']), width="50%")
+            #edit link
+            writeCell('<a href="%s?action=edit&backto=%s">edit</a>' % (entry['Page'], backto)) #request.request_uri[1:]))
+            #remove link
+            writeCell('<a href="%s?action=DeletePage">remove</a>' % entry['Page'])
+
+            request.write('</tr>')
+        
     request.write('</table>')
 
 
 def execute(pagename, request):
+
     request.http_headers()
+    
+    _enter_page(request, pagename)
 
-    plainpage = request.form.get('plainpage', [None])[0]
-
-    if not plainpage == "true":
-	    _enter_page(request, pagename)
-
-    date = request.form.get('date', [None])[0]
+    thisdate = request.form.get('date', [None])[0]
 
     categories = request.form.get('categories', [None])
-
     categories = ','.join(categories)
 
-    globaldata, pagelist, metakeys, styles = metatable_parseargs(request, 'Date=%s,%s' % (date, categories), get_all_keys=True)
+    
+    globaldata, pagelist, metakeys, styles = metatable_parseargs(request, categories, get_all_keys=True)
 
     globaldata.closedb()
 
-    if not pagelist:
-        request.write('No Entries')
-        addEntry(pagename, date, request)
-        return
-
-    events = list()
+    entries = dict()
 
     for page in pagelist:
         metas = getmetas(request, globaldata, page, metakeys, display=False, checkAccess=True)
-        content = Page(request, page).get_raw_body()
-        if '----' in content:
-            content = content.split('----')[0]
-        metas['Page'] = page
-        metas['Content'] = content
-        events.append(metas)
 
-    events.sort(key = lambda x: x['Time'])
+        if u'Date' not in metas.keys():
+            continue
 
-    printEvents(events, date, pagename, request)
+        if metas[u'Date']:
+            date = metas[u'Date'][0][0]
+            datedata = entries.setdefault(date, list())
+            entrycontent = dict()
+            content = Page(request, page).get_raw_body()
+            if '----' in content:
+                content = content.split('----')[0]
+            entrycontent['Content'] = content
+            entrycontent['Page'] = page
 
+            for meta in metas:
+                if not metas[meta]:
+                    continue
+                entrycontent[meta] = metas[meta][0][0]
+            datedata.append(entrycontent)
+
+
+    #Getting current month
+    now = datetime.datetime.strptime(thisdate, '%Y-%m-%d')
+    now = now.replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+    year = now.year
+    month = now.month
+
+    try:
+        end_date = now.replace(month = month + 1, day = 1)
+    except ValueError:
+        end_date = now.replace(month = 1, year = year + 1)
+
+
+    #adding reoccurring events to dict
+
+    new_entries = dict()
+
+    for date in entries.values():
+        for entry in date:
+            if not 'Type' in entry:
+                continue
+
+            type = entry['Type']
+
+            if type == 'Once' or type == '0':
+                continue
+
+            until = None
+            if 'Until' in entry:
+                until = datetime.datetime.strptime(entry['Until'], '%Y-%m-%d')
+                
+            #FIXME tähän niitä weekly ym
+
+            try:
+                type = int(type)
+            except ValueError:
+                continue
+            
+            curdate = datetime.datetime.strptime(entry['Date'], '%Y-%m-%d')
+
+            delta = datetime.timedelta(days = type)
+
+            while curdate <= end_date:
+                if until:
+                    if curdate >= until:
+                        break
+                curdate += delta
+                newdate = curdate.strftime('%Y-%m-%d')
+                
+                new_entries.setdefault(newdate, list())
+                new_entries[newdate].append(entry)
+
+    for entry in new_entries:
+        entries.setdefault(entry, list())
+        entries[entry].extend(new_entries[entry])
+
+    if 'Time' in entries:
+        entries.sort(key = lambda x: x['Time'])
+
+    printEntries(entries, thisdate, pagename, request)
+    
     addEntry(pagename, date, request)
 
-    if not plainpage == "true":
-        _exit_page(request, pagename)
-
+    _exit_page(request, pagename)
+    
