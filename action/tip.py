6aed1b2594ca4a233a0245fa5ac6c5df8c80ff72
ecode-work <ecode@abusesa.com>
2008-09-16 10:25:14 +0000

    fixing #469
    --HG--
    branch : raippa

--- action/tip.py
+++ action/tip.py
@@ -1,9 +1,10 @@
 from MoinMoin.Page import Page
 from graphingwiki.editing import getmetas
-from graphingwiki.patterns import getgraphdata
+from graphingwiki.patterns import getgraphdata, encode
 
 tipcategory = "CategoryTip"
 generictip = u'Your answer was incorrect.'
+checkboxgeneric = u"Your answer was incorrect or you didn't select all the correct answers."
 noanswer = u'You should answer all the questions.'
 recap = u'Your answer was incorrect. Here is some questions to help you out.'
 
@@ -22,7 +23,8 @@ def execute(pagename, request):
         tippagename = "Tip/" + tip
         tippage = Page(request, tippagename)
         if tippage.exists():
-            getgraphdata(request)
+            if not hasattr(request, 'graphdata'):
+                getgraphdata(request)
             metas = getmetas(request, request.graphdata, tippagename, ["WikiCategory", "tip"], checkAccess=False)
             try:
                 category = metas["WikiCategory"][0][0]
@@ -33,7 +35,17 @@ def execute(pagename, request):
         else:
             tiptext = generictip
     else:
+        if not hasattr(request, 'graphdata'):
+            getgraphdata(request)
+        metas = getmetas(request, request.graphdata, encode(pagename), ["question"], checkAccess=False)
         tiptext = generictip
+        if metas["question"]:
+            questionpage = encode(metas["question"][0][0])
+            metas = getmetas(request, request.graphdata, encode(questionpage), ["answertype"], checkAccess=False)
+            for type, metatype in metas["answertype"]:
+               if type == "checkbox":
+                   tiptext = checkboxgeneric
+                   break
 
     tip = u'{{{\nTip: %s\n}}}' % tiptext
 
