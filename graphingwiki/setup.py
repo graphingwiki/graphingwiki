ed684fd5546e79af237208cfc16d61c94a7e1dfd
exec <exec@iki.fi>
2009-02-17 13:34:27 +0000

    Setup changes moin-showpage -> gwiki-showpage etc

--- graphingwiki/setup.py
+++ graphingwiki/setup.py
@@ -5,7 +5,8 @@ old_plugins = {'action': ['ShowProcessGraph.py', 'MetaCSV.py', 'metaCVS.py',
                           'ShowLatexSource.py', 'metaRadarDiagram.py',
                           'metaeditform.py'],
                'macro': ['metaRadarDiagram.py'],
-               'script': ['scripts/gwiki-debuggraph']}
+               'script': ['scripts/gwiki-debuggraph', 'scripts/moin-showpage', 
+                          'scripts/moin-editpage']}
 
 setup(name='graphingwiki', version='0.1',
       author='Juhani Eronen, Joachim Viide, Aki Helin',
@@ -15,8 +16,8 @@ setup(name='graphingwiki', version='0.1',
       package_dir={'graphingwiki': 'graphingwiki'},
       package_data={'graphingwiki': ['plugin/*/*.py', "plugin/__init__.py"]},
       scripts=['scripts/gwiki-rehash', 'scripts/gwiki-showgraph',
-               'scripts/gwiki-install', 'scripts/moin-showpage', 
-               'scripts/mm2gwiki.py', 'scripts/moin-editpage',
+               'scripts/gwiki-install', 'scripts/gwiki-showpage', 
+               'scripts/mm2gwiki.py', 'scripts/gwiki-editpage',
                'scripts/gwiki-xml-attachfile',
                'scripts/gwiki-get-tgz', 'scripts/gwiki-xml-meta'])
 
