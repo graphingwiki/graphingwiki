3967801f3743be25dfca02ede87ab6f9249cd498
Lari Huttunen <github@huttu.net>
2009-02-23 10:45:55 +0000

    Fixed uploadFile()

--- opencollab/opencollab/util/file.py
+++ opencollab/opencollab/util/file.py
@@ -4,8 +4,8 @@ import md5
 import os
 import sys
 
-def hashFile(filename):
-    f = file(filename,'rb')
+def hashFile(file_name):
+    f = file(file_name,'rb')
     hash = md5.new(f.read()).hexdigest()
     f.close();
     return hash
@@ -17,17 +17,17 @@ def uploadFile(collab, page_name, file):
     except IOError:
         sys.exit(IOError)
     parts_uploaded = False
-    for current, total in collab.putAttachmentChunked(page_name, file_name, file):
+    for current, total in collab.putAttachmentChunked(page_name, file_name, file_obj):
         percent = 100.0 * current / float(max(total, 1))
         status = current, total, percent
         sys.stdout.write("\rSent %d/%d bytes (%.02f%%) " % status)
-        sys.stdout.write("of %s" % filename)
+        sys.stdout.write("of %s" % file_name)
         sys.stdout.flush()
         parts_uploaded = True
     if parts_uploaded == True:
         sys.stdout.write("\n")
     else:
-        sys.stdout.write("Already uploaded %s\n" % filename)
+        sys.stdout.write("Already uploaded %s\n" % file_name)
     sys.stdout.flush()
-    file.close()
+    file_obj.close()
     return parts_uploaded
