e8091f5687422752a0b88debcd7d2b2433305383
Lari Huttunen <github@huttu.net>
2009-02-24 18:47:25 +0000

    Basic functionality of opencollab-spam-reader is now complete.

--- opencollab/opencollab/util/file.py
+++ opencollab/opencollab/util/file.py
@@ -3,19 +3,25 @@
 import md5
 import os
 import sys
+import cStringIO
 
-def hashFile(file_name):
-    f = file(file_name,'rb')
-    hash = md5.new(f.read()).hexdigest()
-    f.close();
+def hashFile(f):
+    """
+    Expect a file name or a cStringIO.StringIO object.
+    """
+    try:
+        fobj = file(f,'rb')
+    except(IOError, TypeError):
+        fobj = f
+    hash = md5.new(fobj.read()).hexdigest()
+    fobj.close();
     return hash
 
-def uploadFile(collab, page_name, file):
-    file_name = os.path.basename(file)
+def uploadFile(collab, page_name, file, file_name):
     try:
         file_obj = open(file, "rb")
-    except IOError:
-        sys.exit(IOError)
+    except(IOError, TypeError):
+        file_obj = cStringIO.StringIO(file)
     parts_uploaded = False
     for current, total in collab.putAttachmentChunked(page_name, file_name, file_obj):
         percent = 100.0 * current / float(max(total, 1))
