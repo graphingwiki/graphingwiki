88ae55f7ffa8f84fc7daf846c50fe01eda87adb5
Erno Kuusela <erno@iki.fi>
2009-03-13 15:02:54 +0000

    add GetMetaStruct xml-rpc action for returning metas in idiomatic form

---
+++ graphingwiki/graphingwiki/plugin/xmlrpc/GetMetaStruct.py
@@ -0,0 +1,28 @@
+#! -*- coding: utf-8 -*-"
+"""
+    GetMeta xmlrpc plugin to MoinMoin/Graphingwiki
+     - Sends the Metadata of desired pages
+
+    @copyright: 2007 by Juhani Eronen <exec@iki.fi>
+    @copyright: 2009 by Erno Kuusela <erno@iki.fi>
+    @license: MIT <http://www.opensource.org/licenses/mit-license.php>
+"""
+import urllib
+ 
+from graphingwiki.editing import metatable_parseargs, get_metas
+
+
+def execute(xmlrpcobj, args):
+    request = xmlrpcobj.request
+    _ = request.getText
+
+    # Expects MetaTable arguments
+    pagelist, metakeys, _ = metatable_parseargs(request, args, get_all_keys=True)
+
+    out = {}
+    # We're pretty sure the user has the read access to the pages,
+    # so don't check again
+    for page in pagelist:
+        metas = get_metas(request, page, metakeys, display=False, abs_attach=False, checkAccess=False)
+        out[page] = dict(metas)
+    return out
