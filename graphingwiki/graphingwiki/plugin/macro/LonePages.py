bb1239f867010e4c4a00fb836e14927f590bea64
exec <exec@iki.fi>
2009-03-19 16:29:17 +0000

    Just a quick macro to check out which pages in the wiki have no links, have not been linked to, have not included anything, and, optionally, are not in any hierarchy

---
+++ graphingwiki/graphingwiki/plugin/macro/LonePages.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-"
+"""
+    LonePages macro plugin to MoinMoin
+     - Shows a list of pages that are not connected and not part of a hierarchy
+
+    @copyright: 2009 by Juhani Eronen <exec@iki.fi>
+    @license: MIT <http://www.opensource.org/licenses/mit-license.php>
+"""
+def macro_LonePages(macro, hier=False, docnumber=0):
+    f = macro.formatter
+    request = macro.request
+    _ = request.getText
+
+    out = '<div class="LonePages">' + _('Lone pages')
+
+    out += f.number_list(1, **{'class': 'lonelist'})
+
+    no = 0
+    # Look through pages
+    for page in request.graphdata:
+        # Hier = omit hierarchical
+        if hier:
+            if '/' in page:
+                continue
+
+        if docnumber:
+            if no == docnumber:
+                break
+
+        pageobj = request.graphdata.getpage(page)
+        if (not pageobj.get('out', dict()) and 
+            not pageobj.get('include', dict()) and
+            not pageobj.get('in', dict())):
+            no += 1
+            out += f.listitem(1, **{'class': 'loneitem'}) + \
+                f.pagelink(1, page) + f.text(page) + f.pagelink(0) + \
+                f.listitem(0)
+
+    out += f.number_list(0)
+    out += '</div>'
+
+    return out
