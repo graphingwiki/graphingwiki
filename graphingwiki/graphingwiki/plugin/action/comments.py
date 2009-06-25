a9a6655318f6b317d08639498e7716d3d6e3060c
ecode-work <ecode@abusesa.com>
2009-06-25 10:41:46 +0000

    Initial import of Comments.

---
+++ graphingwiki/graphingwiki/plugin/action/comments.py
@@ -0,0 +1,47 @@
+# -*- coding: iso-8859-1 -*-
+import time
+
+from MoinMoin import wikiutil
+from MoinMoin.Page import Page
+from MoinMoin.user import getUserIdentification
+
+from graphingwiki.editing import set_metas
+
+def save_comment(request, commentpage, sender, comment):
+    comment = u'%s -- %s' % (comment, sender)
+    edittime = time.strftime("%Y-%m-%d %H:%M:%S")
+    metas = {commentpage: {edittime: [comment]}}
+
+    return set_metas(request, dict(), dict(), metas)
+
+def execute(pagename, request):
+    comment = wikiutil.escape(request.form.get('comment', [str()])[0])
+    commentpage = wikiutil.escape(request.form.get('commentpage', [str()])[0])
+
+    if not commentpage:
+        request.theme.add_msg('Missing commentpage.', 'error')
+        Page(request, pagename).send_page()
+        return
+
+    if not comment:
+        request.theme.add_msg('Missing comment.', 'error')
+        Page(request, pagename).send_page()
+        return
+
+    if request.user.exists():
+        sender = '[['+request.user.name+']]'
+    else:
+        sender = getUserIdentification(request)
+
+    try:
+        result, msg = save_comment(request, commentpage, sender, comment)
+        if not result:
+            request.theme.add_msg(msg, 'error')
+            Page(request, pagename).send_page()
+            return
+    except:
+        request.theme.add_msg('Failed to save comment.', 'error')
+        Page(request, pagename).send_page()
+        return
+
+    Page(request, pagename).send_page()
