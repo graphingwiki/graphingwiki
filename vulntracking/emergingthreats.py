a40ebe9cb2fa554f28437160ae646fdeb99e0ea2
exec <exec@iki.fi>
2009-10-02 06:40:26 +0000

    Harmonised emergingthreats, primary key still missing

--- vulntracking/emergingthreats.py
+++ vulntracking/emergingthreats.py
@@ -9,12 +9,28 @@
 
 import urllib, re, socket
 
+from collections import defaultdict
+
 emerging_exploits_url = 'http://www.emergingthreats.net/rules/emerging-exploit.rules'
+
+def fix_refs(refs):
+    new_refs = list()
+    for ref in refs:
+        if ref[0] == 'url':
+            new_refs.append(('URL', 'http://%s' % ref[1]))
+        elif ref[0] == 'cve':
+            new_refs.append(('CVE', 'CVE-%s' % ref[1]))
+        elif ref[0] == 'bugtraq':
+            new_refs.append(('Bugtraq ID', ref[1]))
+            
+    return new_refs
 
 def scrape():
     for line in urllib.urlopen(emerging_exploits_url):
         refs  = re.findall(r'reference:(\w+),([^; ]*)[; ]', line)
-        classtypes  = re.findall(r'classtype:(\w+),([^; ]*)[; ]', line)
+        refs = fix_refs(refs)
+
+        classtypes  = re.findall(r'classtype:([^; ]*)[; ]', line)
         m = re.match(r'alert (\w+) \$\S* any -> \$\S* (\S+)', line)
         if m:
             proto, port = m.groups()
@@ -26,7 +42,14 @@ def scrape():
         else:
             proto, port = None, None
         if refs or classtypes:
-            yield refs, classtypes, (proto, port)
+            zdict = defaultdict()
+            for typ, val in refs:
+                zdict.setdefault(typ, list()).append(val)
+            for x in classtypes:
+                zdict.setdefault('Emergingthreats-classtype', list()).append(x)
+            if proto:
+                zdict['Emergingthreats-target'] = "%s:%s" % (proto.upper(), port)
+            yield zdict
 
 if __name__ == '__main__':
     for z in scrape():
