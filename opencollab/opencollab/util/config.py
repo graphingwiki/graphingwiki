70bf5cd8ad6fc003121744dc53f942de9e87cf5f
Lari Huttunen <github@huttu.net>
2009-11-10 08:06:55 +0000

    More work on parseOptions()

--- opencollab/opencollab/util/config.py
+++ opencollab/opencollab/util/config.py
@@ -20,55 +20,61 @@ def parseConfig(config, *sections):
             iopts[section] = section
     return iopts
 
-def parseOptions(specparser, inisection):
+def parseOptions(specparser, inisection, config=True, category=False, template=False):
     cliopts = {}
     globalopts = {}
     globalopts[inisection] = {}
     globalopts["creds"] = {}
     genparser = copy.deepcopy(specparser)
-    genparser.add_option( "-c", "--config", action="store",
-        type="string", dest="config", default=None,
-        metavar="CONFIG", help="CONFIG file path.")
-    genparser.add_option( "-g", "--category", action="store",
-        type="string", dest="category", default=None,
-        metavar="CONFIG", help="CONFIG file path.")
-    genparser.add_option( "-t", "--template", action="store", 
-        type="string", dest="template", default=None,
-        metavar="TEMPLATE", help="Collab TEMPLATE.")
+    if config:
+        genparser.add_option( "-c", "--config", action="store",
+            type="string", dest="config", default=None,
+            metavar="CONFIG", help="CONFIG file path.")
+    if category:
+        genparser.add_option( "-g", "--category", action="store",
+            type="string", dest="category", default=None,
+            metavar="CONFIG", help="CONFIG file path.")
+    if template:
+        genparser.add_option( "-t", "--template", action="store", 
+            type="string", dest="template", default=None,
+            metavar="TEMPLATE", help="Collab TEMPLATE.")
     genparser.add_option( "-u", "--url", action="store", 
         type="string", dest="url", default=None,
         metavar="COLLABURL", help="COLLABURL to connect to.")
     genparser.add_option("-v", "--verbose", action="store_true", 
         dest="verbose", default=False, help="Enable verbose output." )
+    genparser.set_usage("%prog [options]")
     clivalues, args = genparser.parse_args()
+    # Parse arguments
     if args:
         globalopts[inisection]["args"] = args
     else:
         globalopts[inisection]["args"] = None
+    # Parse CLI options
     for k,v in vars(clivalues).iteritems():
         cliopts[k] = v
-    if clivalues.config:
+    # Parse config
+    if config and clivalues.config:
         iniopts = parseConfig(clivalues.config, "creds", inisection)
         for iopt in iniopts:
             globalopts[iopt] = iniopts[iopt]
-    for k,v in cliopts.iteritems():
-        try: 
-            opt = globalopts[inisection][k]
+    try: 
+        globalopts["creds"]["url"] = cliopts.get("url")
+    except (AttributeError, KeyError):
+        try:
+            globalopts["creds"]["url"]
         except KeyError:
-            if k == "url":
-                if v is not None:
-                    globalopts["creds"][k] = v
-                else:
-                    try:
-                        opt = globalopts["creds"][k]
-                    except KeyError:
-                        globalopts["creds"][k] = v
-            else: 
-                globalopts[inisection][k] = v
+            globalopts["creds"]["url"] = None
+    # Iterate CLI options
+    for k,v in cliopts.iteritems():
+        if k == "url":
+            pass
         else:
-            if v is not None:
-                if k == "url":
-                    globalopts["creds"][k] = v
-                else: 
+            try: 
+                globalopts[inisection][k]
+            except KeyError:
+                globalopts[inisection][k] = v
+            else:
+                if v is not None:
                     globalopts[inisection][k] = v
     return globalopts
