278736119c00afc2e411014285090e6f3db6ac28
Lauri Pokka <larpo@iki.fi>
2009-11-11 18:15:20 +0000

    new question editor with improved file-question support

--- raippa/src/plugin/macro/AnswerBox.py
+++ raippa/src/plugin/macro/AnswerBox.py
@@ -3,10 +3,12 @@ import re, random, time
 
 from MoinMoin.Page import Page
 from MoinMoin.support import difflib
+from MoinMoin import wikiutil
 from graphingwiki.editing import get_metas
-from raippa import removelink, attachment_list, attachment_content
+from raippa import removelink, attachment_list, attachment_content, to_json
 from raippa import raippacategories as rc
-from raippa.pages import Question, Course,  Answer, MissingMetaException, TooManyValuesException
+from raippa.pages import Question, Answer, MissingMetaException, TooManyValuesException
+from raippa.pages import Course
 from raippa.user import User
 
 def sanitize(input):
@@ -96,21 +98,23 @@ def draw_teacherui(macro, user, question):
     res = list()
 
     res.append(f.div(1,id="teacherUiBox"))
-
+    prefix = request.cfg.url_prefix_static
     res.append(f.rawHTML('''
+ <script type="text/javascript" src="%s/raippajs/question_edit.js"></script>
  <script type="text/javascript" src="%s/raippajs/stats.js"></script>
  <script type="text/javascript" src="%s/raippajs/raphael.js"></script>
     <script type="text/javascript">
 
 function editor(view){ 
-    var edit = $('answerEditBox');
+    var editdiv = new Element('div');
     var statsdiv = new Element('div'); 
-    var modal = new modalizer([edit, statsdiv], { 
+    var modal = new modalizer([editdiv, statsdiv], { 
         tabLabels : ["edit", "stats"],
         defTab : view,
         destroyOnExit : false
         });     
     
+    var edit = new QuestionEditor(editdiv, editoptions);
     var stats = new QuestionStats(statsdiv);
     }
 
@@ -118,7 +122,7 @@ function editor(view){
 <br>
 <a class="jslink" onClick="editor(0);">edit</a>
 <a class="jslink" onClick="editor(1);">stats</a>
-''' % (request.cfg.url_prefix_static,request.cfg.url_prefix_static)))
+''' % (prefix, prefix, prefix)))
     #edit ui
     res.extend(draw_answer_edit_ui(macro, question))
 
@@ -129,261 +133,31 @@ function editor(view){
 def draw_answer_edit_ui(macro, question):
     answers = question.answers()
     qtype = question.options().get('answertype', '')
-    redo = question.options().get('redo', '')
+    if question.options().get('redo', False):
+        redo = "true"
+    else:
+        redo = "false"
+
+    if question.options().get('shuffle', False):
+        shuffle = "true"
+    else:
+        shuffle = "false"
+
     request = macro.request
     f = macro.formatter
     res = list()
 
-    res.append(f.rawHTML('''<script type="text/javascript">
-window.addEvent('domready', function(){
-    $try(function(){
-        initAns();
-    });
-    addAns();
-    checkType();
-    clearRms();
-    showTip($$('tr.tiprow').length -1);
-});
-function clearRms(){ 
-    $$('input.rmcheck').each(function(el){
-        el.set('checked', '');
-    });
-    }
 
-function rmRows(){
-    $$('input.rmcheck').each(function(el){
-        if(el.get('checked') == true){
-            $('row'+el.get('value')).destroy();
-            }
-        });
-}
-
-function checkType(){
-   var select = $('typeselect'); 
-   var value = select.get('value');
-   var file_row = $('file_row');
-   var norm_rows = $$('tr[id^=row]');
-   if (value == "file"){
-       file_row.removeClass('hidden');
-       norm_rows.addClass('hidden');
-       clearRms();
-    }else{
-       file_row.addClass('hidden');
-       norm_rows.removeClass('hidden');
-    }
-}
-/* Shows tip and comment fields of <num>th answer*/
-function showTip(num){
-    $$('tr.tiprow').addClass('hidden');
-    $$('tr.comrow').addClass('hidden');
-    if ($('tiprow'+num)){
-        $('tiprow' +num).removeClass('hidden');
-        $('comrow' +num).removeClass('hidden');
-    }
-}
-
-function addAns(ans, val, tip, comment, page){
-    var table = $('ansTable');
-    var tbody = table.getElement('tbody');
-    var checks = table.getElements('input.rmcheck');
-    var num = 0;
-    if (checks.length > 0){
-        num = checks.getLast().get('value').toInt() +1;
-    }
-    var row = new Element('tr',{
-        'id' : 'row' + num,
-        'class' : 'no_border'
-        });
-
-    row.grab(new Element('td')
-        .grab(new Element('input',{
-            'type': 'checkbox',
-            'class' : 'rmcheck',
-            'value' : num,
-            'name' : 'rm'+num
-        })));
-    var tab = new Element('table',{
-            'class' : 'no_border'
-        });
-    var tabTbody = new Element('tbody');
-    tab.grab(tabTbody);
-
-    var ansRow = new Element('tr');
-    var valRow = new Element('tr');
-    var tipRow = new Element('tr',{ 'id': 'tiprow' + num, 'class' : 'tiprow hidden'});
-    var comRow = new Element('tr',{ 'id': 'comrow' + num, 'class' : 'comrow hidden'});
-
-    var ansTd = new Element('td',{
-            'rowspan' : '2'
-            }).grab(new Element('textarea',{
-                    'cols' : 60,
-                    'text' : ans,
-                    'name' : 'answer' + num,
-                    'rows' : 2,
-                    'events' :{
-                        'focus' : function(){
-                            showTip(num);
-                            }
-                        }
-        }));
-    var rightChk = !val || val == "right" ? true : false;
-    var wrongChk = rightChk ?  false : true;
-    var valTd1 = new Element('td')
-            .adopt(new Element('input',{
-                'type' : 'radio',
-                'name' : 'value' +num,
-                'value' : 'right',
-                'id' : 'right' + num,
-                'checked': rightChk
-                }),new Element('label',{
-                    'for': 'right' + num,
-                    'text' : 'right'
-                    }));
-    var valTd2 =  new Element('td')
-            .adopt(new Element('input',{
-                'type' : 'radio',
-                'name' : 'value' +num,
-                'value' : 'wrong',
-                'id' : 'wrong' + num,
-                'checked' : wrongChk
-                }),new Element('label',{
-                    'for': 'wrong' + num,
-                    'text' : 'wrong'
-                    }));
-
-    ansRow.adopt(new Element('th',{
-            'rowspan' : '2',
-            'text' : 'Answer:'
-        }), ansTd, valTd1);
-
-    valRow.grab(valTd2);
-
-    tipRow.adopt(new Element('th',{
-            'text' : 'Tip:'
-        }),new Element('td')
-            .grab(new Element('textarea', {
-                'name' : 'tip' + num,
-                'text' : tip,
-                'cols' : 60,
-                'rows' : 2
-        })));
-
-    comRow.adopt(new Element('th',{
-            'text' : 'Comment:'
-        }),new Element('td')
-            .grab(new Element('textarea', {
-                'name' : 'comment' + num,
-                'text': comment,
-                'cols' : 60,
-                'rows' : 2
-        })));
- 
-    tabTbody.adopt(ansRow, valRow, tipRow, comRow);
-
-    row.grab(new Element('td',{
-        'colspan': 3
-        })
-        .grab(new Element('div')
-            .grab(tab)
-        ));
-
-    if (page){
-        row.grab(new Element('input', {
-            'type' : 'hidden',
-            'name' : 'page' + num,
-            'value' : page
-        }));
-        }
-    tbody.grab(row);
-}
-
-function hideTips(field){
-    var field = $(field);
-    
-
-}
-function submitCheck(button){
-  if(button.value == "Cancel"){
-    return true;
-  }
-  var form = $('editform');
-  var type = $('typeselect').get('value');
-  if(type != 'file'){
-    var ans = $('ansTable').getElements('textarea[name^=answer]');
-    hasAnswer = ans.some(function(a){
-      if(!a.get('name') || a.getParent('tr').hasClass('hidden')){
-        return false;
-      }
-      var name = a.get('name');
-      var value = $(name.replace(/answer/,'right')).checked;
-      var pass = a.value.length > 0  && value;
-      return pass == true;
-    });
-    if(!hasAnswer){
-      var msg = "There is no right answer! Do you still want to save the question?";
-      return confirm(msg);
-    }
-  }else{
-    $('tr[id^=row]').destroy();
-  }
-  //trying to prevent double saves
-  if($(button).retrieve('saved') == 'true'){
-    return false;  
-    }
-
-  $(button).store('saved', 'true');
-  return true;
-}
-
-</script>
-        '''))
-
-    #edit ui
-    res.append(f.div(1,id="answerEditBox", css_class="hidden"))
-
-    res.append(f.rawHTML('<form id="editform" method="post" action="">'))
-    res.append(f.rawHTML('<input type="hidden" name="action" value="editQuestion">'))
-
-    res.append(f.table(1, id="ansTable"))
-    res.append(f.table_row(1))
-    res.append(f.table_cell(1))
-    res.append(f.rawHTML('<a class="jslink" style="color:red;" onclick="rmRows();" title="Remove selected answers">X</a>'))
-    res.append(f.table_cell(0))
-
-    res.append(f.table_cell(1))
-    res.append(f.text("Answer type: "))
-    res.append(f.rawHTML('<select id="typeselect" name="answertype" onchange="checkType();">'))
-    for type in ["checkbox", "radio", "text", "file"]:
-        selected = ""
-        if type == qtype:
-            selected = "selected"
-
-        res.append(f.rawHTML('<option value="%s" %s>%s</option>' %(type,selected, type)))
-
-    res.append(f.rawHTML('</select>'))
-    res.append(f.table_cell(0))
-    res.append(f.table_cell(1))
-    if redo == "True":
-        redo = "checked"
-
-    res.append(f.rawHTML('<input type="checkbox" id="redobox" name="redo" value="True" %s>') % redo)
-    res.append(f.rawHTML('<label for="redobox">Allow redoing question.</label>'))
-    res.append(f.table_cell(0))
-    res.append(f.table_cell(1))
-    res.append(f.rawHTML('<input type="button" value="new answer" onclick="addAns();">'))
-    res.append(f.table_cell(0))
-    res.append(f.table_row(0))
-    
-    file_ans = ""
-
+    ans_js = []
     if qtype in ["checkbox", "radio", "text"]:
     #generating old answers using javascript
-        res.append(f.rawHTML('<script type="text/javascript">function initAns(){'))
         for i, anspage in enumerate(answers):
             answer = Answer(request, anspage)
-            ans = sanitize(answer.answer())
+            ans = answer.answer()
             tips = answer.tips()
-            com = sanitize(answer.comment())
+            com = answer.comment()
+            options = answer.options()
+
             if com == None:
                 com =""
 
@@ -391,31 +165,42 @@ function submitCheck(button){
                 tips =[""]
 
             val = answer.value()
-            tip = sanitize(tips[0])
+            tip = tips[0]
 
-            res.append(f.linebreak(1))
-            res.append(f.rawHTML('''addAns('%s', '%s', '%s', '%s', '%s');''' % (ans, val, tip, com, anspage)))
-
-        res.append(f.rawHTML('}</script>'))
+            ans_js.append(to_json({
+                "name" : ans,
+                "value": val,
+                "tip": tip,
+                "comment": com,
+                "page": anspage,
+                "options": options
+                }))
 
     elif qtype == "file":
-        if len(answers) > 0:
-            answer = Answer(request,answers[0])
-            file_ans = answer.answer()
-        else: 
-            file_ans = u''
+        for i, anspage in enumerate(answers):
+            answer = Answer(request, anspage).answer()
 
-    res.append(f.table_row(1, id="file_row", css_class="no_border"))
-    res.append(f.table_cell(1, colspan="4"))
-    res.append(f.rawHTML('<textarea name="answer" cols="80" rows="30">%s</textarea>' % file_ans))
-    res.append(f.table_cell(0))
-    res.append(f.table_row(0))
+            ans_js.append(to_json({
+                    "name":  answer[0],
+                    "cmd" : answer[1],
+                    "input" : answer[2],
+                    "output" : answer[3],
+                    "infiles" : answer[4],
+                    "outfiles" : answer[5]
+                    }))
 
-    res.append(f.table(0))
-    res.append(f.rawHTML('<input type="submit" onclick="return submitCheck(this);" value="Save">'))
-    res.append(f.rawHTML('<input type="button" onclick="window.location.reload();" value="Cancel">'))
-    res.append(f.rawHTML('</form>'))
-    res.append(f.div(0))
+   #options for editor
+    res.append(f.rawHTML('''
+    <script type="text/javascript">
+    var editoptions = {
+            types :  ["checkbox", "radio", "text", "file"],
+            redo : %s,
+            type: '%s',
+            shuffle: '%s',
+            answers : [%s]
+        };
+    </script>''' % (redo, qtype, shuffle, ",".join(ans_js))))
+    
 
     return res
 
@@ -454,9 +239,15 @@ def draw_answers(macro, user, question):
 
             page = anspage.split("/")[-1]
             
-            value = answer.answer()
             res.append(f.rawHTML(
     '''<input type="%s" id="ans%s" name="answer" value="%s">''' % (qtype, i, page)))
+
+            if 'latex' in answer.options():
+                execute = wikiutil.importPlugin(request.cfg, "macro", 'latex', 'execute')
+                value = execute(macro, answer.answer())
+            else:
+                value = answer.answer()
+
             res.append(f.rawHTML('''<label for="ans%s">%s</label>''' %(i, value)))
             res.append(f.linebreak(0))
 
