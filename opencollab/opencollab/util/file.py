70861dac4cbde3d458eb0a3b0638630c75fff64d
Lari Huttunen <github@huttu.net>
2009-11-14 09:29:12 +0000

    Handle verbosity in a bit more consistent manner with uploading files via uploadFile().

--- opencollab/opencollab/util/file.py
+++ opencollab/opencollab/util/file.py
@@ -20,7 +20,7 @@ def hashFile(f):
         fobj.close()
     return md5.new(data).hexdigest()
 
-def uploadFile(collab, page_name, file, file_name):
+def uploadFile(collab, page_name, file, file_name, verbose=False):
     try:
         file_obj = open(file, "rb")
     except(IOError, TypeError):
@@ -29,14 +29,16 @@ def uploadFile(collab, page_name, file, file_name):
     for current, total in collab.putAttachmentChunked(page_name, file_name, file_obj):
         percent = 100.0 * current / float(max(total, 1))
         status = current, total, percent
-        sys.stdout.write("\rSent %d/%d bytes (%.02f%%) " % status)
-        sys.stdout.write("of %s" % file_name)
-        sys.stdout.flush()
+        if verbose:
+            sys.stdout.write("\rSent %d/%d bytes (%.02f%%) " % status)
+            sys.stdout.write("of %s" % file_name)
+            sys.stdout.flush()
         parts_uploaded = True
-    if parts_uploaded == True:
-        sys.stdout.write("\n")
-    else:
-        sys.stdout.write("Already uploaded %s\n" % file_name)
+    if verbose:
+        if parts_uploaded == True:
+            sys.stdout.write("\n")
+        else:
+            sys.stdout.write("Already uploaded %s\n" % file_name)
     sys.stdout.flush()
     file_obj.close()
     return parts_uploaded
