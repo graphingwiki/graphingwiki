cffe25a3a19a2b7cdc2f6ddb7354dddccaf4afd2
Rauli Leevi Sakari Puuperä <therauli@therau.li>
2009-11-30 12:45:40 +0000

    HasDone now checks if user/page exists

--- raippa/src/plugin/xmlrpc/HasDone.py
+++ raippa/src/plugin/xmlrpc/HasDone.py
@@ -2,27 +2,37 @@
 """
     HasDone.py -- Tells if user has done a given task/question
 """
-import urllib
- 
-from raippa.user import User
+import xmlrpclib
+
+from MoinMoin.Page import Page
+from MoinMoin.user import getUserId
+
+from raippa.user import RaippaUser
 from raippa.pages import Task, Question
-from graphingwiki.editing import get_metas
 from raippa import raippacategories as rc
+
+from graphingwiki.editing import get_metas
+
 
 def execute(xmlrpcobj, username, pagename):
     request = xmlrpcobj.request
     _ = request.getText
+
+    if not Page(reqeust, pagename).exists():
+        return xmlrpclib.Fault(1, _('No such page %s' % pagename))
+
+    if not getUserId(request, username):
+        return xmlrpclib.Fault(1, _('No such user %s' % username))    
     
+    user = RaippaUser(request, username)
+
     metas = get_metas(request, pagename, ['gwikicategory'])
-    
-    user = User(request, username)
 
     if rc['question'] in metas['gwikicategory']:
         page = Question(request, pagename)
-
-        return user.hasdone(user, page)
     elif rc['task']:
         page = Task(request, pagename)
-        return user.hasdone(user, page)
+    else:
+        return  xmlrpclib.Fault(1, _('Page %s not in question or task category' % pagename))
 
-    return None, None
+    return user.has_done(page)
