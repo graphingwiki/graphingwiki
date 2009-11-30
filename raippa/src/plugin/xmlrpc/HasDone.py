3908ed6d3fbd6380518604431aec3eae7c6adaaf
Rauli Leevi Sakari Puuperä <therauli@therau.li>
2009-11-30 12:17:17 +0000

    Added hasdone for cheking user doneness.

---
+++ raippa/src/plugin/xmlrpc/HasDone.py
@@ -0,0 +1,28 @@
+#! -*- coding: utf-8 -*-"
+"""
+    HasDone.py -- Tells if user has done a given task/question
+"""
+import urllib
+ 
+from raippa.user import User
+from raippa.pages import Task, Question
+from graphingwiki.editing import get_metas
+from raippa import raippacategories as rc
+
+def execute(xmlrpcobj, username, pagename):
+    request = xmlrpcobj.request
+    _ = request.getText
+    
+    metas = get_metas(pagename)
+    
+    user = User(request, username)
+
+    if rc['question'] in metas.values()['gwikicategory']:
+        page = Question(request, pagename)
+
+        return user.hasdone(user, page)
+    elif rc['task']:
+        page = Task(request, pagename)
+        return user.hasdone(user, page)
+
+    return None, None
