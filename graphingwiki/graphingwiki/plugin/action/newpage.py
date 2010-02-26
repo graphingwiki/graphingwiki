d17cf8170185b09185f60e6731d137daf7c69097
exec <exec@iki.fi>
2010-02-26 16:18:44 +0000

    newpage had problems with mod_wsgi - seems this was somehow related to request copying. Looking at the code, I saw no reasons for the copy, so I removed it.

--- graphingwiki/graphingwiki/plugin/action/newpage.py
+++ graphingwiki/graphingwiki/plugin/action/newpage.py
@@ -8,14 +8,13 @@
 """
 from copy import copy
 from MoinMoin.action.newpage import NewPage
+from MoinMoin import log
 
 def execute(pagename, request):
     # Editaction allows for arbitrary action to be used for editing target page
     editaction = request.form.get('editfunc', [''])[0]
     # Edit actions assumed to be all in lower case
     editaction = editaction.lower()
-    # Making a throwaway request by copying to avoid littering modifications
-    newreq = copy(request)
 
     if editaction:
         # MoinMoin.action.newpage left no choice but to edit the URL
@@ -24,8 +23,8 @@ def execute(pagename, request):
         
         def redirect_editaction(url):
             url = url.replace('action=edit', 'action=%s' % editaction)
-            newreq.http_redirect_real(url)
+            request.http_redirect_real(url)
 
-        newreq.http_redirect = redirect_editaction
+        request.http_redirect = redirect_editaction
 
-    return NewPage(newreq, pagename).render()
+    return NewPage(request, pagename).render()
