67b4a386d13edc1bb2326ec6b26b2f299ca958c6
Lauri Pokka <larpo@iki.fi>
2010-11-15 11:42:10 +0000

    raippa: fix for #772

--- raippa/src/plugin/action/showCalendarDate.py
+++ raippa/src/plugin/action/showCalendarDate.py
@@ -69,20 +69,18 @@ def printEntries(entries, date, pagename, request):
         request.write(stuff)
         request.write('</td>')
     
-    request.write(u'''<script type="text/javascript"
-    src="%s/raippajs/mootools-1.2-core-yc.js"></script>
-    <script type="text/javascript">
-    function toggle(el){
+    request.write(u'''<script type="text/javascript">
+    function toggleParticipants(el){
         var part_list = $(el).getParent('p').getNext('div');
         part_list.toggleClass('hidden');
         if(el.text == "hide"){
             el.set('text', 'show');
         }else{
-            el.set('text', 'hide');    
+            el.set('text', 'hide');
         }
     }
     </script>
-    '''% request.cfg.url_prefix_static )
+    ''')
     request.write(u'<table id="calEventList">')
     request.write(u'''<tr><th colspan="6">%s</th></tr>
     <tr>
@@ -130,7 +128,7 @@ def printEntries(entries, date, pagename, request):
                participant_list = "<br>".join(participants)
                participant_div = '''<p><span style="float:left;">%s /
                <b>%s</b></span><span style="float:right;margin-left:5px;"><a class="jslink" 
-               onclick="toggle(this);">show</a></span><br
+               onclick="toggleParticipants(this);">show</a></span><br
                clear="all"></p><div class="hidden">%s</div>
                ''' % (len(participants), capacity, participant_list)
             else:
