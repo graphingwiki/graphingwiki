c2b9143436c91b000e4db1123f3ce1e4bdf09e1b
exec <exec@iki.fi>
2011-08-02 14:50:53 +0300

    Changed example config to match current JavaScript includes

--- collab/config/collabfarm.py
+++ collab/config/collabfarm.py
@@ -147,7 +147,8 @@ class FarmConfig(DefaultConfig):
     ''' % {"url_prefix_static": url_prefix_static}
 
     for script in ['js/mootools-core-yc.js', \
-                   'js/sorttable-moo.js', \
+                   'js/mootools-more-yc.js', \
+                   'js/gwiki-common.js', \
                    'js/mootree.js', \
                    'simile/timeline/timeline_ajax/simile-ajax-api.js', \
                    'simile/timeline/timeline_js/timeline-api.js?bundle=true']:
