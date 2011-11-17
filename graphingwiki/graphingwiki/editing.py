9ab07797b483ad359a198a8a9f0b4774caf348c0
exec <exec@iki.fi>
2011-11-17 10:02:02 +0200

    Added logging to attachment handling. ChunkedAttachFile logged already by default.

--- graphingwiki/graphingwiki/editing.py
+++ graphingwiki/graphingwiki/editing.py
@@ -1573,13 +1573,16 @@ def load_attachfile(request, pagename, aname):
 
     return adata
 
-def delete_attachfile(request, pagename, aname):
+def delete_attachfile(request, pagename, aname, log=False):
     try:
         fpath, exists = check_attachfile(request, pagename, aname)
         if not exists:
             return False
 
         os.unlink(fpath)
+
+        if log:
+            _addLogEntry(request, 'ATTDEL', pagename, aname)
     except:
         return False
 
