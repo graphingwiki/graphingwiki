cb0c6b60e6fcec974c3f7ee040a8c745c9d8eaf3
Lauri Pokka <larpo@iki.fi>
2012-02-29 16:49:44 +0200

    Adding Invite macro that has support for custom invite templates and button texts. Usage example:
    Create drop-down menu with two different template values to choose from.
    Template argument takes three options (template for new users, template for old users, label for drop down menu) that are separated by semicolon.
    <<Invite(template=InviteNewTemplate;InviteOldTemplate;original, template=InviteNewTemplate2;InviteOldTemplate2;custom)>>
    
    Use custom template and show own text on invite button and label.
    <<Invite(template=InviteNewTemplate2;InviteOldTemplate2, text=Invite with custom template, button_text=do it!)>>

--- graphingwiki/graphingwiki/plugin/action/Invite.py
+++ graphingwiki/graphingwiki/plugin/action/Invite.py
@@ -16,16 +16,22 @@ GROUP_DEFAULT_VARIABLE = "invite_group_default"
 GROUP_DEFAULT_DEFAULT = ""
 
 class Invite(ActionBase):
-    def __init__(self, pagename, request):
+    def __init__(self, pagename, request, **kw):
         ActionBase.__init__(self, pagename, request)
         self.getText = request.getText
         self.request = request
         self.use_ticket = True
         self.form_trigger = 'invite'
-        self.form_trigger_label = self.getText('Invite')
+        self.form_trigger_label = wikiutil.escape(kw.get('button_text', [self.getText('Invite')])[0], True)
+        self.template = kw.get('template', [])
+        self.querytext = kw.get('text', [self.getText('Invite user to visit this page')])[0]
 
     def _load_template(self, variable, default):
-        name = getattr(self.request.cfg, variable, default)
+        if not variable and default:
+            name = default
+        else:
+            name = getattr(self.request.cfg, variable, default)
+
         if not self.request.user.may.read(name):
             raise InviteException("You are not allowed to read template page '%s'." % name)
 
@@ -48,6 +54,14 @@ class Invite(ActionBase):
         return user_may_invite(self.request.user, self.pagename)
 
     def do_action(self):
+        template = self.form.get('template', [''])[0]
+        template = wikiutil.clean_input(template).strip().split(',')
+        new_template = old_template = None
+        if len(template) > 0:
+            new_template = template[0]
+            if len(template) > 1:
+                old_template = template[1]
+
         email = self.form.get('email', [u''])[0]
         email = wikiutil.clean_input(email).strip()
         if len(email) == 0:
@@ -55,8 +69,15 @@ class Invite(ActionBase):
 
         pagename = self.pagename
         try:
-            new_template = self._load_template(NEW_TEMPLATE_VARIABLE, NEW_TEMPLATE_DEFAULT)
-            old_template = self._load_template(OLD_TEMPLATE_VARIABLE, OLD_TEMPLATE_DEFAULT)
+            if new_template:
+                new_template = self._load_template(None, new_template)
+            else:
+                new_template = self._load_template(NEW_TEMPLATE_VARIABLE, NEW_TEMPLATE_DEFAULT)
+
+            if old_template:
+                old_template = self._load_template(None, old_template)
+            else:
+                old_template = self._load_template(OLD_TEMPLATE_VARIABLE, OLD_TEMPLATE_DEFAULT)
 
             if wikiutil.isGroupPage(self.request, pagename):
                 myuser = invite_user_to_wiki(self.request, pagename, email, new_template, old_template)
@@ -80,11 +101,35 @@ class Invite(ActionBase):
         return True, cgi.escape("Invitation mail sent to address '%s'." % email)
 
     def get_form_html(self, buttons_html):
+        template_html = ''
+
+        for template in self.template:
+            if type(template) == type(list()):
+                value = name = template[0]
+                if len(template) > 1:
+                    value += "," + template[1]
+                if len(template) > 2:
+                    name = template[2]
+            else:
+                value = name = template
+
+            value = wikiutil.escape(value, True)
+            name = wikiutil.escape(name)
+
+            if len(self.template) > 1:
+                template_html += '''\n<option value="%s">%s</option>''' %(value, name)
+            else:
+                template_html = '<input type="hidden" name="template" value="%s">' %(value)
+
+        if len(self.template) >1:
+              template_html = '''<select name="template">%s</select>''' %(template_html)
+
         d = {
             'pagename': self.pagename,
             'email_label': self.getText("Email:"),
             'buttons_html': buttons_html,
-            'querytext': self.getText('Invite user to visit this page'),
+            'querytext': wikiutil.escape(self.querytext),
+            'template_html': template_html,
         }
         return '''
 <strong>%(querytext)s</strong>
@@ -92,7 +137,7 @@ class Invite(ActionBase):
     <tr>
         <td class="label"><label>%(email_label)s</label></td>
         <td class="content">
-            <input type="text" name="email" maxlength="200">
+            <input type="text" name="email" maxlength="200">%(template_html)s
         </td>
     </tr>
     <tr>
