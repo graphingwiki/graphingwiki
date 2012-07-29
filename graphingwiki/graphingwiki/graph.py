175f3995ea707943aa0661601cbbab5ab20a4e4d
exec <exec@iki.fi>
2012-07-29 17:02:33 +0300

    Wiki pages with the key update could not be rendered as graphs. This is due to an overlap with the method update of the AttrBag class in Graph.py. This commit tries to solve all similar overlaps by introducing namespaces to the AttrBag attributes.

--- graphingwiki/graphingwiki/graph.py
+++ graphingwiki/graphingwiki/graph.py
@@ -45,7 +45,8 @@ class AttrBag(object):
     def __init__(self, **keys):
         object.__init__(self)
 
-        self._ignored = set()
+        self.__dict__['_ignored'] = set()
+        self.__dict__['_namespace'] = self.__class__.__name__ + '.'
         self._ignored.update(self.__dict__)
 
         self.__dict__.update(keys)
@@ -56,12 +57,33 @@ class AttrBag(object):
         for key in variables:
             if key in self._ignored:
                 continue
-            yield key, self.__dict__[key]
+            keyname = self._remove_namespace(key)
+            yield keyname, self.__dict__[key]
+
+    def _add_namespace(self, key):
+        return self.__dict__['_namespace'] + key
+
+    def _remove_namespace(self, key):
+        return key.replace(self.__dict__['_namespace'], '', 1)
+
+    def _in_namespace(self, key):
+        return key.startswith(self.__dict__['_namespace'])
+    
+    def __getattr__(self, key):
+        nskey = self._add_namespace(key)
+        try:
+            return self.__dict__[nskey]
+        except KeyError:
+            raise AttributeError("%r object has no attribute %r" %
+                                 (self.__class__.__name__, key))
 
     def __setattr__(self, key, value):
         # HACK
         if isinstance(key, unicode):
             key = encode_page(key)
+        # HACK HACK
+        if not self._in_namespace(key):
+            key = self._add_namespace(key)
         self.__dict__[key] = value
 
     def update(self, other):
@@ -73,7 +95,7 @@ class Node(AttrBag):
         # Set the _identity attribute before calling the __init__ of
         # the super class, because we don't want it returned when
         # e.g. iterating.
-        self._identity = identity
+        self.__dict__['_identity'] = identity
         AttrBag.__init__(self, **keys)
 
     def __unicode__(self):
@@ -256,8 +278,8 @@ class Graph(Node):
     []
     """
     def __init__(self):
-        self.nodes = Nodes(self)
-        self.edges = Edges()
+        self.__dict__['nodes'] = Nodes(self)
+        self.__dict__['edges'] = Edges()
         Node.__init__(self, '')
 
     def __nonzero__(self):
