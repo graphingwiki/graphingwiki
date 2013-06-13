d68a151c44405012573acc990427318cbfd1a19a
Ossi Salmi <osalmi@iki.fi>
2013-06-13 14:14:58 +0300

    collab: Add support for ejabberd authentication using session id
    collab/collabbackend/plugin/action/authcredentials.py:
    
      Returns user's JID and current session id as JSON.
    
    collab/scripts/collab-auth-ejabberd:
    
      Now tries to authenticate using session id if password auth fails.

---
+++ collab/collabbackend/plugin/action/authcredentials.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-"
+
+import re
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+def execute(pagename, request):
+    request.content_type = "application/json"
+
+    sid = request.session.sid
+    user = request.user.name.replace("@", "%")
+    if hasattr(request.cfg, "collab_chat_domain"):
+        domain = request.cfg.collab_chat_domain
+    else:
+        domain = re.sub(r"^www\.", "", request.host)
+
+    if None not in (sid, user, domain):
+        data = {
+            "jid": "%s@%s" % (user, domain),
+            "password": sid,
+        }
+    else:
+        data = {}
+
+    json.dump(data, request)
