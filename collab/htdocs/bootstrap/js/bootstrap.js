bd89eeea45e2618e1ec049a342c933dab7f60ca5
Lauri Pokka <larpo@iki.fi>
2013-11-29 19:32:46 +0200

    bootstrap.js: add collapse toggle support.

--- collab/htdocs/bootstrap/js/bootstrap.js
+++ collab/htdocs/bootstrap/js/bootstrap.js
@@ -1,21 +1,57 @@
 /*
-     bootstrap.js
-     A drop in replacement for bootstrap Dropdown
-     License: MIT <http://www.opensource.org/licenses/mit-license.php>
-     Copyright: 2013 by Lauri Pokka
+ bootstrap.js
+ A drop in replacement for bootstrap Dropdown
+ License: MIT <http://www.opensource.org/licenses/mit-license.php>
+ Copyright: 2013 by Lauri Pokka
  */
 document.id(window).addEvent('domready', function() {
-    var toggle = "[data-toggle=dropdown]";
+    "use strict";
+
     var body = document.id(document.body);
-    body.addEvent('click:relay(' + toggle + ')', function(e) {
+    body.addEvent('click:relay([data-toggle=dropdown])', function(e) {
         e.preventDefault();
         var parent = this.getParent();
 
         if (!parent.hasClass('open')) {
             parent.addClass('open');
-            body.addEvent('click:once', function(){
+            body.addEvent('click:once', function() {
                 parent.removeClass('open');
             })
         }
+    });
+
+    body.addEvent('click:relay([data-toggle=collapse])', function(e) {
+        $$(this.get('data-target')).forEach(function(parent) {
+            var show = !parent.retrieve('show');
+
+            if (!parent.retrieve('height')) {
+                parent.store('height', parent.getDimensions()['height']);
+            }
+
+            parent.store('show', show)
+                .removeClass('collapse')
+                .addClass('collapsing')
+                .addClass('in');
+
+            if (show) {
+                parent.setStyle.delay(0, parent, ['height', parent.retrieve('height')]);
+            } else {
+                parent.setStyle('height', 'auto');
+            }
+
+            var done = function() {
+                parent.removeClass('collapsing');
+                if (!parent.retrieve('show')) {
+                    parent.removeClass('in');
+                    parent.addClass('collapse');
+                }
+            };
+
+            if ("ontransitionend" in window) {
+                parent.addEventListener('transitionend', done);
+            } else {
+                done();
+            }
+        });
     });
 });
