3eae06d29814ad4baf6db832c75c8e3a8506ef67
Joachim Viide <jviide@iki.fi>
2014-01-06 15:46:28 +0200

    collab/collabbackend/plugin/macro/CollabChat.py: Merge changes from pull request #7 manually.

--- collab/collabbackend/plugin/macro/CollabChat.py
+++ collab/collabbackend/plugin/macro/CollabChat.py
@@ -3,36 +3,44 @@ import random
 SCRIPT = """
 <script>
 (function() {
-  var request = new Request.JSON({
-    "url": "%(creds)s",
+    var request = new Request.JSON({
+        "url": "%(creds)s",
 
-    "onSuccess": function(creds) {
-      initChat("%(id)s", "%(bosh)s", "%(room)s", creds.jid, creds.password);
-    },
+        "onSuccess": function(creds) {
+            initChat("%(id)s", "%(bosh)s", "%(room)s", creds.jid, creds.password);
+        },
 
-    "onFailure": function() {
-      alert("JSON request failed.");
-    }
-  });
+        "onFailure": function() {
+            alert("JSON request failed.");
+        }
+    });
 
-  request.send();
+    request.send();
 })();
 </script>
 """
 
 def macro_CollabChat(self, args):
+    room = [self.request.cfg.interwikiname]
+    room_mask = [maskable.lower() for maskable in getattr(self.request.cfg, "collabchat_room_mask", [])]
+    subroom_id = self.request.page.page_name.lower()
 
-  room = self.request.cfg.interwikiname
-  bosh = self.request.cfg.collab_chat_bosh
-  creds = self.request.cfg.collab_chat_creds
+    if subroom_id:
+        subroom_id = [".".join(subroom_id.replace(".", "").split("/"))]
+        subrooms = set(subroom_id) - set(room_mask)
+        room.extend(subrooms)
 
-  id = "chat-%x" % random.randint(2 ** 63, 2 ** 64 - 1)
+    bosh = self.request.cfg.collab_chat_bosh
+    creds = self.request.cfg.collab_chat_creds
 
-  result = list()
-  result.append(self.formatter.div(1, **{ "id": id, "class": "collab_chat" }))
-  result.append(self.formatter.div(0))
+    id = "chat-%x" % random.randint(2 ** 63, 2 ** 64 - 1)
 
-  script = SCRIPT % { "bosh": bosh, "room": room, "creds": creds, "id": id }
-  result.append(script)
+    result = list()
+    result.append(self.formatter.div(1, **{ "id": id, "class": "collab_chat" }))
+    result.append(self.formatter.div(0))
 
-  return "".join(result)
+    script = SCRIPT % { "bosh": bosh, "room": ".".join(room), "creds": creds, "id": id }
+    result.append(script)
+
+    return "".join(result)
+
