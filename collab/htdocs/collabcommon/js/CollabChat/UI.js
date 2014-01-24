c3de679be4e8a1091f039ffd930ccb48eabb6a55
Tomi Juntunen <erani@iki.fi>
2014-01-24 15:49:18 +0200

    collab/htdocs/collabcommon/js/CollabChat/UI.js: Use preventWheelGesture event from dom module.

--- collab/htdocs/collabcommon/js/CollabChat/UI.js
+++ collab/htdocs/collabcommon/js/CollabChat/UI.js
@@ -167,10 +167,10 @@ define([
             _this.isAtBottom = (this.scrollTop + this.clientHeight) === this.scrollHeight;
         });
 
-        dom.listen(this.areaContainer, "mousewheel", this.preventWheelGestures.bind(this));
-        dom.listen(this.areaContainer, "wheel", this.preventWheelGestures.bind(this));
-        dom.listen(this.userlistContainer, "mousewheel", this.preventWheelGestures.bind(this));
-        dom.listen(this.userlistContainer, "wheel", this.preventWheelGestures.bind(this));
+        dom.listen(this.areaContainer, "mousewheel", dom.preventWheelGestures.bind(this));
+        dom.listen(this.areaContainer, "wheel", dom.preventWheelGestures.bind(this));
+        dom.listen(this.userlistContainer, "mousewheel", dom.preventWheelGestures.bind(this));
+        dom.listen(this.userlistContainer, "wheel", dom.preventWheelGestures.bind(this));
 
         dom.listen(window, "resize", function() {
             if (_this.isAtBottom) {
@@ -281,48 +281,6 @@ define([
 
     UI.prototype.setChannelLabel = function(label) {
         this.channelLabel.textContent = label;
-    };
-
-    UI.prototype.preventWheelGestures = function(event, _element) {
-        var element = _element || event.currentTarget;
-
-        var deltaX = 0;
-        var deltaY = 0;
-        if (event.type === "mousewheel") {
-            deltaX = -(event.wheelDeltaX || 0);
-            deltaY = -(event.wheelDeltaY || (deltaX === 0 ? event.wheelDelta : 0)) || 0;
-        } else if (event.type === "wheel") {
-            deltaX = event.deltaX;
-            deltaY = event.deltaY;
-        }
-
-        var left = element.scrollLeft;
-        var ignoreX =
-            (deltaX === 0) ||
-            (deltaX > 0 && left >= element.scrollWidth - element.clientWidth) ||
-            (deltaX < 0 && left <= 0);
-
-        var top = element.scrollTop;
-        var ignoreY =
-            (deltaY === 0) ||
-            (deltaY > 0 && top >= element.scrollHeight - element.clientHeight) ||
-            (deltaY < 0 && top <= 0);
-
-        if (!ignoreX && deltaX > 0 && left === element.scrollWidth - element.clientWidth - 1) {
-            element.scrollLeft += 1;
-            ignoreX = true;
-        }
-        if (!ignoreY && deltaY > 0 && top === element.scrollHeight - element.clientHeight - 1) {
-            element.scrollTop += 1;
-            ignoreY = true;
-        }
-
-        if (ignoreX && ignoreY) {
-            event.stopPropagation();
-            event.preventDefault();
-            return false;
-        }
-        return true;
     };
 
     return UI;
