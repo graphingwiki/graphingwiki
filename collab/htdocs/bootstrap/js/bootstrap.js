ef99e8f3c18f9da4ac8b061d8413af9894361340
Lauri Pokka <larpo@iki.fi>
2014-01-31 18:19:17 +0200

    collab/htdocs/bootstrap/js/bootstrap.js: Remove unnecessary dom-ready event.

--- collab/htdocs/bootstrap/js/bootstrap.js
+++ collab/htdocs/bootstrap/js/bootstrap.js
@@ -5,55 +5,53 @@
  Copyright: 2013 by Lauri Pokka
  */
 require(['mootools'], function() {
-    document.id(window).addEvent('domready', function() {
-        "use strict";
+    "use strict";
 
-        var body = document.id(document.body);
-        body.addEvent('click:relay([data-toggle=dropdown])', function(e) {
-            e.preventDefault();
-            var parent = this.getParent();
+    var body = document.id(document.body);
+    body.addEvent('click:relay([data-toggle=dropdown])', function(e) {
+        e.preventDefault();
+        var parent = this.getParent();
 
-            if (!parent.hasClass('open')) {
-                parent.addClass('open');
-                body.addEvent('click:once', function() {
-                    parent.removeClass('open');
-                })
+        if (!parent.hasClass('open')) {
+            parent.addClass('open');
+            body.addEvent('click:once', function() {
+                parent.removeClass('open');
+            })
+        }
+    });
+
+    body.addEvent('click:relay([data-toggle=collapse])', function(e) {
+        $$(this.get('data-target')).forEach(function(parent) {
+            var show = !parent.retrieve('show');
+
+            if (!parent.retrieve('height')) {
+                parent.store('height', parent.getDimensions()['height']);
             }
-        });
 
-        body.addEvent('click:relay([data-toggle=collapse])', function(e) {
-            $$(this.get('data-target')).forEach(function(parent) {
-                var show = !parent.retrieve('show');
+            parent.store('show', show)
+                .removeClass('collapse')
+                .addClass('collapsing')
+                .addClass('in');
 
-                if (!parent.retrieve('height')) {
-                    parent.store('height', parent.getDimensions()['height']);
+            if (show) {
+                parent.setStyle.delay(0, parent, ['height', parent.retrieve('height')]);
+            } else {
+                parent.setStyle('height', 'auto');
+            }
+
+            var done = function() {
+                parent.removeClass('collapsing');
+                if (!parent.retrieve('show')) {
+                    parent.removeClass('in');
+                    parent.addClass('collapse');
                 }
+            };
 
-                parent.store('show', show)
-                    .removeClass('collapse')
-                    .addClass('collapsing')
-                    .addClass('in');
-
-                if (show) {
-                    parent.setStyle.delay(0, parent, ['height', parent.retrieve('height')]);
-                } else {
-                    parent.setStyle('height', 'auto');
-                }
-
-                var done = function() {
-                    parent.removeClass('collapsing');
-                    if (!parent.retrieve('show')) {
-                        parent.removeClass('in');
-                        parent.addClass('collapse');
-                    }
-                };
-
-                if ("ontransitionend" in window) {
-                    parent.addEventListener('transitionend', done);
-                } else {
-                    done();
-                }
-            });
+            if ("ontransitionend" in window) {
+                parent.addEventListener('transitionend', done);
+            } else {
+                done();
+            }
         });
     });
 });
