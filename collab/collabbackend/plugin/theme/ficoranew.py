306fe391510411993fd618b8ebe64ef2b566ea96
exec <exec@iki.fi>
2014-02-09 00:44:13 +0200

    collab theme ficoranew: Updated logo, styles to combine opencollabnew and ficora

--- collab/collabbackend/plugin/theme/ficoranew.py
+++ collab/collabbackend/plugin/theme/ficoranew.py
@@ -1,15 +1,21 @@
 from MoinMoin.theme import modernized as basetheme
 from opencollabnew import Theme as ThemeParent
 
+NAME = "ficoranew"
+
 class Theme(ThemeParent):
-    name = "ficoranew"
+    name = NAME
+
+    def __init__(self, request):
+        ThemeParent.__init__(self, request)
+        self.stylesheets = self.stylesheets + (('screen', 'screen'),)
 
     def footer_string(self):
         if hasattr(self.cfg, 'footer_string'):
             footer_string = self.cfg.footer_string
         else:
             footer_string = \
-            u'  <p class="footertext">CERT-FI<br>PL 313<br>00181 Helsinki'+ \
+            u'  <p class="footertext">Kyberturvallisuuskeskus<br>PL 313<br>00181 Helsinki'+ \
                 '<br>Tel. +358 (0)295 390 230</p>'
 
         return footer_string
@@ -20,5 +26,5 @@ class Theme(ThemeParent):
             mylogo = u'<a href="' + \
                 self.request.script_root + '/' + self.request.cfg.page_front_page + \
                 '"><img src="' + self.cfg.url_prefix_static + \
-                '/ficoranew/img2/cert-fi.png" alt="CERT-FI"></a>'
+                '/ficoranew/img2/kyberturvallisuus_neg_rgb.png" alt="CERT-FI"></a>'
         return mylogo
