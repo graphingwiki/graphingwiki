56ca6c7e60cc034fea3f77bdb693578dcb31c8a8
Lauri Pokka <larpo@iki.fi>
2014-03-21 17:36:59 +0200

    opencollab theme: New collab-list js widget that opens by clicking on the navbar brand and shows all collabs that user has access to.

--- collab/htdocs/bootstrap/js/bootstrap.js
+++ collab/htdocs/bootstrap/js/bootstrap.js
@@ -4,7 +4,10 @@
  License: MIT <http://www.opensource.org/licenses/mit-license.php>
  Copyright: 2013 by Lauri Pokka
  */
-require(['mootools'], function() {
+require([
+    'collabcommon/common/dom',
+    'mootools'
+], function(dom) {
     "use strict";
 
     var body = document.id(document.body);
@@ -29,7 +32,7 @@ require(['mootools'], function() {
             parent.addClass('open');
             body.addEvent('click:once', function() {
                 parent.removeClass('open');
-            })
+            });
         }
     });
 
@@ -69,4 +72,67 @@ require(['mootools'], function() {
             }
         });
     });
+
+    $$('.navbar-brand').addEvent('click', function(e) {
+        if ($$('div.collab-list-container').length) {
+            return;
+        }
+
+        e.preventDefault();
+
+        var cont = new Element('div.collab-list-container.list-group.loading').inject(document.body);
+        var spinner = new Element('i.glyphicon.glyphicon-refresh.icon-spin');
+        cont.grab(new Element('div.spinner-container').grab(spinner));
+        dom.listen(cont, "mousewheel", dom.preventWheelGestures);
+        dom.listen(cont, "wheel", dom.preventWheelGestures);
+
+        new Request.JSON({
+            url: '?action=collabListJSON',
+            onSuccess: function(collabs) {
+                cont.empty();
+                var filter = new Element('input[placeholder=Filter]');
+                cont.grab(new Element('span.list-group-item').grab(filter));
+                filter.focus();
+
+                filter.addEventListener('keyup', function(e){
+                    var val = this.value;
+                    var filtered = collabs.filter(function(collab){
+                        return (collab.title + collab.motd + collab.shortName).indexOf(val) != -1;
+                    });
+
+                    list(filtered);
+                });
+
+                var list = function(items) {
+                    cont.getElements('a').destroy();
+                    items.forEach(function(collab) {
+                        var active = collab.active ? " active" : "";
+                        cont.grab(new Element('a', {
+                            'class': 'list-group-item' + active,
+                            'href': collab.url
+                        }).adopt(
+                                new Element('h4.list-group-item-heading', {text: collab.title}),
+                                new Element('p.list-group-item-text', {
+                                        text: collab.motd}
+                                )
+                            ));
+                    });
+                };
+
+                list(collabs);
+
+            }
+        }).get();
+
+        var close = function(e) {
+            if (!e.target.getParent('.collab-list-container')) {
+                $$('div.collab-list-container').destroy();
+                body.removeEvent('click', close);
+            }
+        };
+        body.addEventListener('click', close);
+
+        return false;
+    });
+
 });
