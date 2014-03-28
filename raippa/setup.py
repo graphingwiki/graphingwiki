ff213277c7555522f1c067d1642028eab15f89f0
ecode-work <ecode@abusesa.com>
2014-03-28 14:55:56 +0200

    raippa: Removed.

--- raippa/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-setup(name='raippa',
-    packages=['raippa'],
-    package_dir={'raippa': 'src'},
-    package_data={'raippa': ['plugin/*/*.py']},
-    scripts=['scripts/raippa-install']
-    )
