13f54fac4c93241b2c2f16df4a56756fd7f07eee
Mika Seppänen <mika.seppanen@iki.fi>
2015-02-23 13:02:30 +0200

    graphingwiki/graphingwiki/plugin/macro/GetMetaData.py: Switch from _sysmsg() to render_error().

--- graphingwiki/graphingwiki/plugin/macro/GetMetaData.py
+++ graphingwiki/graphingwiki/plugin/macro/GetMetaData.py
@@ -6,10 +6,8 @@
     @copyright: 2007 by Juhani Eronen <exec@iki.fi>
     @license: MIT <http://www.opensource.org/licenses/mit-license.php>
 """
-from MoinMoin.macro.Include import _sysmsg
-
 from graphingwiki.editing import get_metas
-from graphingwiki.util import format_wikitext
+from graphingwiki.util import format_wikitext, render_error
 
 Dependencies = ['metadata']
 
@@ -22,8 +20,7 @@ def execute(macro, args):
         args = [x.strip() for x in args.split(',')]
     # Wrong number of arguments
     if not args or len(args) not in [1, 2]:
-        return _sysmsg % ('error',
-                          _("GetMetaData: Need to specify page, or page and key"))
+        return render_error( _("GetMetaData: Need to specify page, or page and key"))
 
     # Get all non-empty args
     args = [x for x in args if x]
@@ -37,8 +34,7 @@ def execute(macro, args):
         key = args[1]
     # Faulty args
     else:
-        return _sysmsg % ('error',
-                          _("GetMetaData: Need to specify page, or page and key"))
+        return render_error(_("GetMetaData: Need to specify page, or page and key"))
 
     vals = get_metas(request, page, [key], formatLinks=True)
 
