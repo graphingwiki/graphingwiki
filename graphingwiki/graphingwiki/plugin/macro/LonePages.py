b511a93d7f47df84f5132e24ead1c175ef837afe
Mika Seppänen <mika.seppanen@iki.fi>
2015-02-24 15:52:40 +0200

    graphingwiki/graphingwiki/plugin/macro/LonePages.py: Add permission checks, rename arguments to be more self documenting, clean-up code and add documentation.

--- graphingwiki/graphingwiki/plugin/macro/LonePages.py
+++ graphingwiki/graphingwiki/plugin/macro/LonePages.py
@@ -1,43 +1,57 @@
 # -*- coding: utf-8 -*-"
 """
-    LonePages macro plugin to MoinMoin
-     - Shows a list of pages that are not connected and not part of a hierarchy
+    Shows a list of pages that are not connected.
+
+    Show all pages that are not connected:
+
+    <<LonePages>>
+
+    Filter away pages that are part of hierarchy:
+
+    <<LonePages(omit_hierarchical=True)>>
+
+    Show only first 10 matches:
+
+    <<LonePages(limit=10)>>
 
     @copyright: 2009 by Juhani Eronen <exec@iki.fi>
+                2015 by Mika Seppänen <mika.seppanen@iki.fi>
     @license: MIT <http://www.opensource.org/licenses/mit-license.php>
 """
-def macro_LonePages(macro, hier=False, docnumber=0):
+
+
+def macro_LonePages(macro, omit_hierarchical=False, limit=0):
     f = macro.formatter
     request = macro.request
-    _ = request.getText
 
-    out = '<div class="LonePages">' + _('Lone pages')
+    out = f.div(1, **{"class": "lonepages"})
+    out += f.number_list(1, **{"class": "lonelist"})
 
-    out += f.number_list(1, **{'class': 'lonelist'})
+    count = 0
 
-    no = 0
-    # Look through pages
     for page in request.graphdata:
-        # Hier = omit hierarchical
-        if hier:
+        if not request.user.may.read(page):
+            continue
+
+        if omit_hierarchical:
             if '/' in page:
                 continue
 
-        if docnumber:
-            if no == docnumber:
+        if limit:
+            if count >= limit:
                 break
 
         pageobj = request.graphdata.getpage(page)
-        if (pageobj.get('saved') and
-            not pageobj.get('out', dict()) and 
-            not pageobj.get('include', dict()) and
-            not pageobj.get('in', dict())):
-            no += 1
-            out += f.listitem(1, **{'class': 'loneitem'}) + \
-                f.pagelink(1, page) + f.text(page) + f.pagelink(0) + \
-                f.listitem(0)
+        if (pageobj.get("saved") and
+           not pageobj.get("out") and
+           not pageobj.get("include") and
+           not pageobj.get("in")):
+            count += 1
+            out += (f.listitem(1, **{"class": "loneitem"}) +
+                    f.pagelink(1, page) + f.text(page) + f.pagelink(0) +
+                    f.listitem(0))
 
     out += f.number_list(0)
-    out += '</div>'
+    out += f.div(0)
 
     return out
