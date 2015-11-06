c9c741cecc170dd60e320eeb90775479089a1d19
exec <exec@iki.fi>
2015-11-06 09:25:47 +0200

    ficorasimple theme: simplified search box

--- collab/collabbackend/plugin/theme/ficorasimple.py
+++ collab/collabbackend/plugin/theme/ficorasimple.py
@@ -12,3 +12,24 @@ class Theme(ThemeParent):
         Remove the wrench menu
         """
         return u""
+
+    def search_form(self, d):
+        _ = self.request.getText
+        url = self.request.href(d['page'].page_name)
+
+        return u"""
+  <form method="get" class="navbar-right navbar-form" action="%s">
+    <div class="form-group">
+      <div class="input-group">
+        <input type="hidden" name="action" value="fullsearch">
+        <input type="hidden" name="context" value="180">
+        <input type="hidden" name="titlesearch" value="0">
+        <input type="text" autocomplete="off" class="form-control search" placeholder="Search" name="value">
+        <div class="input-group-btn">
+            <button class="btn btn-primary" name="titlesearch" type="submit">
+                <i class="glyphicon glyphicon-search"></i>
+            </button>
+        </div>
+      </div>
+    </div>
+  </form>""" % url
