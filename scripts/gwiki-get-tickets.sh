ae6522dd5267b3d436d687e6a7fe75f065467669
Ossi Salmi <osalmi@iki.fi>
2016-11-17 13:32:04 +0200

    scripts/gwiki-get-tickets.sh: Remove obsolete script

--- scripts/gwiki-get-tickets.sh
+++
@@ -1,5 +0,0 @@
-wget http://graphingwiki.python-hosting.com/report/1 -O report.html
-mkdir -p ticket
-perl -pi -e 's!/ticket/!./ticket/!' report.html 
-grep /ticket/ report.html | cut -d '/' -f 3 | cut -d '"' -f 1 | sort | uniq | \
-xargs -I {} wget http://graphingwiki.python-hosting.com/ticket/{} -O ticket/{}
